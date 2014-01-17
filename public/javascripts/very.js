6082beeddf531ecca88090a580f904c405792bf0
William Silversmith <william.silversmith@gmail.com>
2014-01-16 23:23:49 -0500

    Whoa!!!

--- public/javascripts/very.js
+++ public/javascripts/very.js
@@ -1,42 +1,84 @@
-$(document).ready(function() {
+;(function ($, undefined) {
 
-	$('#share').alwaysCenterIn(document, {
-		top: -50,
-	});
-	
-	setTimeout(function () { 
-		shakaroo($('#share')); 
-	}, 500);
+	var _audioclips = [];
+	var next_clip = 0;
 
-	setInterval(function() {
-		shakaroo($('#share'));
-	}, Math.round(Math.random() * (7000) + 5000));
+	var NUMCLIPS = 8;
 
-	$('#share').mouseenter(function(){
-		$(this).fadeTo('fast', .5);
+	$(document).ready(function() {
+		shareButtonConfiguration();
+		configureAudio();
 	});
 
-	$('#share').mouseleave(function(){
-		$(this).fadeTo('fast', 1);
-	});
+	function configureAudio () {
+		for (var i = 0; i < 5; i++) {
+			var random = random_integer(NUMCLIPS) + 1;
+			random = random.toFixed(0);
 
-	$('#share').click(function(){
-		$(this).fadeOut('fast');
-	});
-});
+			var clip = new Audio("/audio/veryshare-" + random + ".wav");
+			clip.preload = true;
 
-function shakaroo (element) {
-	var bool = Math.round(Math.random()); // true/false
+			clip.addEventListener('canplay', (function (bound_clip) {
+				return function () {
+					_audioclips.push(bound_clip);
+				};
+			})(clip));
+		}
 
-	var direction = bool 
-		? 'up' 
-		: 'left';
+		$('#share').click(function () {
+			if (_audioclips.length === 0) {
+				return;
+			}
+
+			var clipindex = next_clip % _audioclips.length;
+			var selectedclip = _audioclips[clipindex];
+			next_clip++; // must occur before play otherwise there's a race condition
+
+			selectedclip.play();
+		});
+	}
+
+	function shareButtonConfiguration () {
+		$('#share').alwaysCenterIn(document, {
+			top: -50,
+		});
 		
-	$(element).effect('shake', {
-		times: 3, 
-		distance: 3,
-		direction: direction,
-	}, 25);
-}
+		setTimeout(function () { 
+			shakaroo($('#share')); 
+		}, 500);
 
+		setInterval(function() {
+			shakaroo($('#share'));
+		}, Math.round(Math.random() * (7000) + 5000));
 
+		$('#share').mouseenter(function(){
+			$(this).fadeTo('fast', .5);
+		});
+
+		$('#share').mouseleave(function(){
+			$(this).fadeTo('fast', 1);
+		});
+
+		$('#share').click(function(){
+			$(this).fadeOut('fast');
+		});
+	}
+
+	function shakaroo (element) {
+		var bool = Math.round(Math.random()); // true/false
+
+		var direction = bool 
+			? 'up' 
+			: 'left';
+			
+		$(element).effect('shake', {
+			times: 3, 
+			distance: 3,
+			direction: direction,
+		}, 25);
+	}
+
+	function random_integer (max) {
+		return Math.round(Math.random() * 10000) % max;
+	}
+})(jQuery);
