466ba5cccbd166f6b403d754881f694d61c6729a
alexnortn <alexnortn@gmail.com>
2014-01-27 01:50:48 -0500

    Fixed audio levels and working on submitclips.

---
+++ public/javascripts/voice.js
@@ -0,0 +1,12 @@
+(function ($, undefined) {
+
+
+	$(document).ready(function () {
+		$('#mainContainer').first().slideme({
+			arrows: true,
+			speed: 5000,
+			transition: 'page'
+		});
+	});
+})(jQuery);
+
